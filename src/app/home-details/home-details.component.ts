9f41bdbd8e2c8fce8d09728ec8977baa0e24f59d
krishantha.dharmasiri@outlook.com <krishantha.dharmasiri@outlook.com>
2019-07-14 15:42:26 +1000

    Added a function to show a snap shot of the component on the right side pane once the component is clicked on the left side

--- src/app/home-details/home-details.component.ts
+++ src/app/home-details/home-details.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-details.component.css']
 })
 export class HomeDetailsComponent implements OnInit {
+  imageSrc:string="/assets/comment.png";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private toggleImage(identifier:number):void{
+    if(identifier == 1)
+      this.imageSrc = "/assets/comment.png";
+    else if(identifier == 2)
+      this.imageSrc = "/assets/comment-tile.png";
+    
   }
 
 }
